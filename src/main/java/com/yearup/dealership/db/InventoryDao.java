65be5dd68b05402444d5afaf3a841687f0e24ffd
Brandon <brandonjohnson.bj880@gmail.com>
2024-12-07 10:19:50 -0500

    Fix issues within my addVehicleInventory method. Now working on the logic of my remmove VehicleFrominventory

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -3,6 +3,7 @@ package com.yearup.dealership.db;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class InventoryDao {
@@ -12,19 +13,31 @@ public class InventoryDao {
         this.dataSource = dataSource;
     }
 
-    public void addVehicleToInventory(String VIN, int dealershipId) {
-        // TODO: Implement the logic to add a vehicle to the inventory
+    public void addVehicleToInventory(String VIN, int dealership_id) {
+        // I am checking if the dealership_id exists in the dealership table
+        // By doing this I am only specify if the SELECT row 1 has a Dealership_id column exists.
+        String checkDealershipSQL = "SELECT 1 FROM dealerships WHERE dealership_id = ?";
         try (Connection connect = dataSource.getConnection();
-        PreparedStatement statement = connect.prepareStatement(
-                "INSERT INTO vehicles (VIN, dealership_id) values (?,?)")) {
-            statement.setString(1,VIN);
-            statement.setInt(2, dealershipId);
+             PreparedStatement checkStatement = connect.prepareStatement(checkDealershipSQL)) {
 
-            statement.executeUpdate();
-
+            checkStatement.setInt(1, dealership_id);
+            try (ResultSet rs = checkStatement.executeQuery()) {
+                if (rs.next()) {
+                    // Dealership exists, proceed to insert the vehicle
+                    String insertSQL = "INSERT INTO inventory (VIN, dealership_id) VALUES (?, ?)";
+                    try (PreparedStatement statement = connect.prepareStatement(insertSQL)) {
+                        statement.setString(1, VIN);
+                        statement.setInt(2, dealership_id);
+                        statement.executeUpdate();
+                    }
+                } else {
+                    System.out.println("Error: Dealership with ID " + dealership_id + " does not exist.");
+                }
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+
     }
 
     public void removeVehicleFromInventory(String vin) {
